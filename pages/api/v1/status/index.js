59211102d0c49ab17353cef822ff255c151bbe11
Dinobergue Viana <dinobergueviana@gmail.com>
2024-02-06 02:01:42 +0000

    finish /status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,7 +2,32 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
-  response.status(200).json({ updated_at: updatedAt });
+
+  const databseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionsValue =
+    databaseMaxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOnpendConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOnpendConnectionsValue =
+    databaseOnpendConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opend_connections: databaseOnpendConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
