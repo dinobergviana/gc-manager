ce5913cf3381c1bdc09fd93cbba36270ba52c2a4
dinobergviana <dinobergueviana@gmail.com>
2025-06-12 08:04:42 -0300

    feat: create sessions and authetication features

--- infra/controller.js
+++ infra/controller.js
@@ -3,15 +3,15 @@ import {
   MethodNotAllowedErrorError,
   ValidationError,
   NotFoundError,
+  UnauthorizedError,
 } from "infra/errors";
 
 function onErrorHandler(error, request, response) {
-  if (error instanceof ValidationError || error instanceof NotFoundError) {
+  if (error instanceof ValidationError || error instanceof NotFoundError || error instanceof UnauthorizedError) {
     return response.status(error.statusCode).json(error);
   }
 
   const publicErrorbject = new InternalServerError({
-    statusCode: error.statusCode,
     cause: error,
   });
 
